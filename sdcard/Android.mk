ede2e8cb0946f86ad4fb1d0e1b8b3062b5aec50a
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-11-02 20:35:53 +0000

    Small 4.4 fixes

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -8,11 +8,9 @@ LOCAL_CFLAGS := -Wall -Wno-unused-parameter
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_STATIC_LIBRARY)
 
-LOCAL_SHARED_LIBRARIES := libcutils
-
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := main.c
 LOCAL_MODULE := sdcard
 LOCAL_CFLAGS := -Wall -Wno-unused-parameter
-LOCAL_STATIC_LIBRARIES := libsdcard
+LOCAL_STATIC_LIBRARIES := libsdcard libc libcutils liblog
 include $(BUILD_EXECUTABLE)
